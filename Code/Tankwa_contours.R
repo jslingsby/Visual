a46206ff2d6934b8bc382c96dd0e2921e780ffaa
jslingsby <jslingsby@gmail.com>
2016-03-22 09:26:46 +0200

    Tweaked Tankwa_contours to do a test run of different options for Tanqua map.

--- Code/Tankwa_contours.R
+++ Code/Tankwa_contours.R
@@ -9,26 +9,56 @@ library(raster)
 
 dem <- raster("/Users/jasper/Documents/GIS/ASTER30m/tankwa_March2016.tif")
 
-nex <- extent(19.06666667, 20.8833333, -33.4333333, -31.333333)
+#nex <- extent(19.06666667, 20.8833333, -33.4333333, -31.333333)
+nex <- extent(20, 20.25, -32.25, -32)
 
 dem <- crop(dem, nex)
 
 NAvalue(dem) <- -32768
 
 #Basic 20m contours
-pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_500Mp.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)))
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_20In.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)), colormodel="cmyk")
+contour(dem, levels = seq(0,2300,20), drawlabels=F, lwd=0.05, maxpixels=5000000)
+dev.off()
+
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_80In.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)), colormodel="cmyk")
+contour(dem, levels = seq(0,2300,20), drawlabels=F, lwd=0.05, maxpixels=5000000)
+dev.off()
+
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_50Mp_20In.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)), colormodel="cmyk")
+contour(dem, levels = seq(0,2300,20), drawlabels=F, lwd=0.05, maxpixels=50000000)
+dev.off()
+
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_50Mp_80In.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)), colormodel="cmyk")
 contour(dem, levels = seq(0,2300,20), drawlabels=F, lwd=0.05, maxpixels=50000000)
 dev.off()
 
 # darkslategrey 20m contours with thicker black 100m
 x <- seq(20,2300,20)
 x<- x[-which(x %in% seq(100,2300,100))]
-pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_500Mp.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)))
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_20In_2Col.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)), colormodel="cmyk")
 contour(dem, levels = x, drawlabels=F, lwd=0.05, maxpixels=50000000, col="darkslategray")
-contour(dem, levels = seq(100,2300,100), drawlabels=F, lwd=0.1, maxpixels=50000000)
+contour(dem, levels = seq(100,2300,100), drawlabels=F, lwd=0.1, maxpixels=50000000, add=T)
 dev.off()
 
-pdf("Output/Tankwa_30mASTERdem_50mContours_0.05Line_500Mp.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)))
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_80In_2Col.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)), colormodel="cmyk")
+contour(dem, levels = x, drawlabels=F, lwd=0.05, maxpixels=5000000, col="darkslategray")
+contour(dem, levels = seq(100,2300,100), drawlabels=F, lwd=0.1, maxpixels=5000000, add=T)
+dev.off()
+
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_50Mp_20In_2Col.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)), colormodel="cmyk")
+contour(dem, levels = x, drawlabels=F, lwd=0.05, maxpixels=50000000, col="darkslategray")
+contour(dem, levels = seq(100,2300,100), drawlabels=F, lwd=0.1, maxpixels=50000000, add=T)
+dev.off()
+
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_50Mp_80In_2Col.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)), colormodel="cmyk")
+contour(dem, levels = x, drawlabels=F, lwd=0.05, maxpixels=50000000, col="darkslategray")
+contour(dem, levels = seq(100,2300,100), drawlabels=F, lwd=0.1, maxpixels=50000000, add=T)
+dev.off()
+
+######
+
+pdf("Output/Tankwa_30mASTERdem_50mContours_0.05Line_50Mp.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)))
 contour(dem, levels = seq(0,2300,50), drawlabels=F, lwd=0.05, maxpixels=50000000)
 dev.off()
 
