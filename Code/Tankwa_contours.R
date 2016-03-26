18081f4beee3303802e9299852be9ff7c5adbfe6
jslingsby <jslingsby@gmail.com>
2016-03-26 21:59:01 +0200

    Played with smoothing contours by smoothing the dem using focal() in raster

--- Code/Tankwa_contours.R
+++ Code/Tankwa_contours.R
@@ -16,10 +16,20 @@ dem <- crop(dem, nex)
 
 NAvalue(dem) <- -32768
 
+#demSmooth <- aggregate(x, 1)
+demSmooth <- focal(dem, w=matrix(1, 3, 3), mean)
+#options("max.contour.segments" = 1000)
+
 #Basic 20m contours
 pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_20In.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)), colormodel="cmyk")
 contour(dem, levels = seq(0,2300,20), drawlabels=F, lwd=0.05, maxpixels=5000000)
 dev.off()
+
+#Basic 20m contours (smoothed)
+pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_20In_Smoothed.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)), colormodel="cmyk")
+contour(demSmooth, levels = seq(0,2300,20), drawlabels=F, lwd=0.05, maxpixels=5000000)
+dev.off()
+
 
 pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_80In.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)), colormodel="cmyk")
 contour(dem, levels = seq(0,2300,20), drawlabels=F, lwd=0.05, maxpixels=5000000)
@@ -37,8 +47,8 @@ dev.off()
 x <- seq(20,2300,20)
 x<- x[-which(x %in% seq(100,2300,100))]
 pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_20In_2Col.pdf", width = 20, height = 20*(nrow(dem)/ncol(dem)), colormodel="cmyk")
-contour(dem, levels = x, drawlabels=F, lwd=0.05, maxpixels=50000000, col="darkslategray")
-contour(dem, levels = seq(100,2300,100), drawlabels=F, lwd=0.1, maxpixels=50000000, add=T)
+contour(dem, levels = x, drawlabels=F, lwd=0.05, maxpixels=5000000, col="darkslategray")
+contour(dem, levels = seq(100,2300,100), drawlabels=F, lwd=0.1, maxpixels=5000000, add=T)
 dev.off()
 
 pdf("Output/Tankwa_30mASTERdem_20mContours_0.05Line_5Mp_80In_2Col.pdf", width = 80, height = 80*(nrow(dem)/ncol(dem)), colormodel="cmyk")
